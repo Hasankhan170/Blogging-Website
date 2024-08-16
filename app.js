8775ffa926f04451288795ee571050bcb526784d
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-15 22:04:30 -0700

    working

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { auth } from "./config.js";
 
+
+// const login = document.querySelector('.login')
+ 
+
+// onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//         console.log(user);
+
+
+//     } else {
+//         login.innerHTML = `<button><a href="./login.html"></a>login</button>`
+
+//     }
+//   });
 
--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,5 @@
 import {collection,addDoc, getDocs, doc, deleteDoc ,updateDoc } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js'
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"
-import{db ,auth} from "./config.js"
+import{ db } from "./config.js"
 
 
 
@@ -8,6 +7,31 @@ const dashboardForm = document.querySelector('.dashboard-form')
 const dashboardInput = document.querySelector('.dashboard-input')
 const dashboardTextArea = document.querySelector('.dashboard-textarea')
 const renderBlogs = document.querySelector('.renderBlogs')
+
+let arr = []
+
+dashboardForm.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+
+    const userValues = {
+        title: dashboardInput.value,
+        content: dashboardTextArea.value,
+    }
+
+    try {
+        const docRef = await addDoc(collection(db, "blogs"), userValues);
+        arr.push(userValues)
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+
+       dashboardInput.value = ''
+       dashboardTextArea.value = ''
+    
+})
+
+
 
 
 
